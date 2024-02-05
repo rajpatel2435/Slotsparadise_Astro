83e69c8f19fb58a6277688e3955dc18da511d620
Jenny Martinez <lajennylove@users.noreply.github.com>
2024-02-05 14:16:56 -0500

    feat: nodejs server | WPDB-2417

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
-
 import netlify from "@astrojs/netlify/functions";
+
+
+//https://docs.astro.build/en/guides/integrations-guide/sitemap/
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
