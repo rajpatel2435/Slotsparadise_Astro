de9fc824cefa1acec896fcee78aeb894235bfb65
sarbjeet <sabi521@gmail.com>
2024-03-22 12:55:32 -0400

    endpoints updated

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -1,5 +1,4 @@
 export const [GRAPHQL_ENDPOINT, ADMIN_AJAX_ENDPOINT] = [
-    "https://wordpress-1221271-4344842.cloudwaysapps.com/graphql",
-    "https://wordpress-1221271-4344842.cloudwaysapps.com/wp-admin/admin-ajax.php"
-  ];
-  
+  "https://slotsparadise.com/wp-graphql",
+  "https://slotsparadise.com/wp-admin/admin-ajax.php",
+];
--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,4 +1,4 @@
-// Function to set a cookie
+/* // Function to set a cookie
 function setCookie(cookieName, cookieValue, expirationDays) {
     var d = new Date();
     d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
@@ -18,70 +18,67 @@ function deleteCookie(cookieName) {
 
 // Usage: delete the "l_i" cookie
 deleteCookie("l_i");
-
-
+ */
 
 // Function to check cookies and update CSS
 function checkCookiesAndUpdateCSS() {
-    //console.log("hello");
-    //console.log("Cookies:", document.cookie); // Log all cookies to check their names and values
-    if (document.cookie.split(";").some((item) => item.trim().startsWith("l_i="))) {
-        //console.log("second hello"); // Log if the condition is met
-        
-        // Update CSS properties for elements with class "modal"
-        var modals = document.querySelectorAll(".modal");
-        modals.forEach(function(modal) {
-            modal.style.display = "block";
-        });
+  //console.log("hello");
+  //console.log("Cookies:", document.cookie); // Log all cookies to check their names and values
+  if (
+    document.cookie.split(";").some((item) => item.trim().startsWith("l_i="))
+  ) {
+    //console.log("second hello"); // Log if the condition is met
 
-        // Update CSS properties for elements with class "modal"
-        var usdBtn = document.querySelectorAll(".usd-btn");
-        usdBtn.forEach(function(btn) {
-            btn.style.display = "inline-block";
-        });
+    // Update CSS properties for elements with class "modal"
+    var modals = document.querySelectorAll(".modal");
+    modals.forEach(function (modal) {
+      modal.style.display = "block";
+    });
 
-        // Update CSS properties for elements with class "modal"
-        var depBtn = document.querySelectorAll(".dep-btn");
-        depBtn.forEach(function(btn) {
-            btn.style.display = "inline-block";
-        });
+    // Update CSS properties for elements with class "modal"
+    var usdBtn = document.querySelectorAll(".usd-btn");
+    usdBtn.forEach(function (btn) {
+      btn.style.display = "inline-block";
+    });
 
-        // Update CSS properties for elements with class "modal"
-        var loginBtn = document.querySelectorAll(".login-btn");
-        loginBtn.forEach(function(btn) {
-            btn.style.display = "none";
-        });
+    // Update CSS properties for elements with class "modal"
+    var depBtn = document.querySelectorAll(".dep-btn");
+    depBtn.forEach(function (btn) {
+      btn.style.display = "inline-block";
+    });
 
-        // Update CSS properties for elements with class "modal"
-        var joinBtn = document.querySelectorAll(".join-btn");
-        joinBtn.forEach(function(btn) {
-            btn.style.display = "none";
-        });
+    // Update CSS properties for elements with class "modal"
+    var loginBtn = document.querySelectorAll(".login-btn");
+    loginBtn.forEach(function (btn) {
+      btn.style.display = "none";
+    });
 
-        // Update CSS property for elements with class "Menu--users"
-        var menuUsers = document.querySelectorAll(".Menu--users");
+    // Update CSS properties for elements with class "modal"
+    var joinBtn = document.querySelectorAll(".join-btn");
+    joinBtn.forEach(function (btn) {
+      btn.style.display = "none";
+    });
 
-        // Select the join_usd container
+    // Update CSS property for elements with class "Menu--users"
+    var menuUsers = document.querySelectorAll(".Menu--users");
+
+    // Select the join_usd container
     var join_usd = document.querySelector(".join-usd-container");
 
     // Add mouseover event listener to show menuUsers
-    join_usd.addEventListener("mouseover", function() {
-        menuUsers.forEach(function(menu) {
-            menu.style.display = "block";
-        });
+    join_usd.addEventListener("mouseover", function () {
+      menuUsers.forEach(function (menu) {
+        menu.style.display = "block";
+      });
     });
 
     // Add mouseout event listener to hide menuUsers
-    join_usd.addEventListener("mouseout", function() {
-        menuUsers.forEach(function(menu) {
-            menu.style.display = "none";
-        });
+    join_usd.addEventListener("mouseout", function () {
+      menuUsers.forEach(function (menu) {
+        menu.style.display = "none";
+      });
     });
-
-    
-    }
-
-    
+  }
 }
 
 // Call the function when the DOM is loaded
