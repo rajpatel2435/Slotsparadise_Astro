f0a3593e5d6d0b3d96ffaab7116504fc5ecd2931
sarbjeet <sabi521@gmail.com>
2024-04-18 10:57:16 -0400

    chnages to get casino news on home page using ajax

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -1,4 +1,4 @@
 export const [GRAPHQL_ENDPOINT, ADMIN_AJAX_ENDPOINT] = [
-  "http://localhost/wordpress/graphql",
-  "http://localhost/wordpress/wp-admin/admin-ajax.php",
+  "https://slotsparadise.com/wp-graphql",
+  "https://slotsparadise.com/wp-admin/admin-ajax.php",
 ];
