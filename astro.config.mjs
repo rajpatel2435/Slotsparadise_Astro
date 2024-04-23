8bec11433af03507b78d82a2b2a1b7eb6f537cbf
sarbjeet <sabi521@gmail.com>
2024-04-23 09:59:40 -0400

    chnaged img width for sidebar article css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/the-beach',
+  base: "/the-beach",
   trailingSlash: "ignore",
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
   server: {
     port: 4321,
-    host: '127.0.0.1'
+    host: "127.0.0.1",
+  },
+  redirects: {
+    "/keno": {
+      status: 302,
+      destination: "/other-games",
+    },
   },
 });
