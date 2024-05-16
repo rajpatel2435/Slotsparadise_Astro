a0e8062762a5834410498cfe00fa31aa1f8caed7
Nidhi Kairo <nkairo@CLTBCANADA.net>
2024-05-16 13:07:16 -0400

    fix: updated htaccess rules for trailing slash and update code to generate seo meta tags correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
+
 export default defineConfig({
   integrations: [tailwind()],
   base: "/the-beach",
-  trailingSlash: "ignore",
+  trailingSlash: "always",
   output: "server",
   adapter: node({
     mode: "standalone",
@@ -14,10 +15,5 @@ export default defineConfig({
     port: 4321,
     host: "127.0.0.1",
   },
-  redirects: {
-    "/keno": {
-      status: 302,
-      destination: "/other-games",
-    },
-  },
+ 
 });
