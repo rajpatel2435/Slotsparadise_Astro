3aef41900aba331cb2276ada468e073580399ab7
Nidhi Kairo <nkairo@CLTBCANADA.net>
2024-05-17 13:06:42 -0400

    fix: updated changes - page titles, mutliple h2, urls

---
+++ src/scripts/imagesData.js
@@ -0,0 +1,18 @@
+// home-page top casino games section images 
+import blackjack from "../assets/home-page-images/slots-paradise-thebeach-blackjack.webp";
+import slots from "../assets/home-page-images/slots-paradise-thebeach-slots-1.webp";
+import rouletteImage from "../assets/home-page-images/slots-paradise-thebeach-roulette.webp";
+import baccaratImage from "../assets/home-page-images/slots-paradise-thebeach-baccarat.webp";
+
+
+//top casino game images
+const casinoImages = [
+    {path:blackjack}, //0
+    {path:slots}, //1
+    {path:rouletteImage}, //2
+    {path:baccaratImage}, //3
+  ]
+
+
+  
+  export {casinoImages};
