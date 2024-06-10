dae93d6ecc00a7713e6c1dde4ac7c96cfe3bc222
Nidhi Kairo <nkairo@CLTBCANADA.net>
2024-06-10 16:55:16 -0400

    fix: updated endopint file, astro config and installed cross-env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Load environment variables from the chosen .env file
+const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production';
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+// Ensure GRAPHQL_ENDPOINT is set in process.env
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+
 
 export default defineConfig({
   integrations: [tailwind()],
