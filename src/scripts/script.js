0ab95ab07ec4a64ab3b1bdc2625f864aaf77f84f
Nidhi Kairo <nkairo@CLTBCANADA.net>
2024-10-31 13:54:19 -0400

    chore: added balance updater script for shared header and footer

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -22,3 +22,126 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+//balance updater script for shared header and footer
+var BalanceUpdater = {
+  PlayerId: 0,
+  _intervalId: null,
+  logOutCount: 0,
+  _isRunning: false,
+
+start: function () {
+  if (this._isRunning)
+      return;
+
+  this._intervalId = setInterval(this.getBalanceAmount.bind(this), 10000);
+  this._isRunning = true;
+},
+
+tryUpdateStatus: function () {
+  var t = this;
+
+  if (this._isRunning) {
+      return;
+  }
+
+  if (t._intervalId) {
+      clearTimeout(t._intervalId);
+  }
+
+  $.ajax({
+      url: '/Common/GetLoginStatus',
+      type: 'post',
+      dataType: 'json',
+      success: function (data) {
+          if ((typeof regClicked === 'undefined' || !regClicked) && data && data.Reload == "Reload") {
+              document.location.reload();
+          } else {
+              t._intervalId = setTimeout(t.tryUpdateStatus.bind(t), 20000);
+          }
+      }
+  });
+},
+
+stop: function () {
+  clearInterval(this._intervalId);
+  this._isRunning = false;
+},
+
+
+
+// balance
+getBalanceAmount: function () {
+  if (this.PlayerId > 0) {
+
+      var obj = {
+          id: this.PlayerId,
+          gameId: $('#ProductId').val() || 1
+      };
+      var data = $.param(obj);
+
+      
+      document.cookie = 'l_i=' + $.param({ gameId: obj.gameId, id: this.PlayerId }) + '; expires=' + moment().add(1, 'day').toDate().toString() + '; path=/;SameSite=Lax';
+
+      if (this.logOutCount > 0) {
+          data = $.param({});
+          document.cookie = 'l_i=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      }
+
+      $.ajax({
+          url: '/common/getinfo?' + data,
+          type: 'post',
+          dataType: 'json',
+          success: this.onBalanceStatus.bind(this)
+      });
+  }
+},
+
+onBalanceStatus: function (data) {
+
+  if (data.Balance) {
+      this.logOutCount = 0;
+
+      var $balanceViewer = $('#menu-button-balance');
+      if ($balanceViewer.length) {
+          $balanceViewer.text(' ' + data.Balance);;
+      } else {
+          $balanceViewer = $('.Play--Balance');
+          $balanceViewer.text(' ' + data.Balance);
+      }
+
+      $('#menu-played-balance').text(' ' + data.PlayedBalance);
+      $('#menu-notplayed-balance').text(' ' + data.NotPlayedBalance);
+      $('#menu-withdrawable-balance').text(' ' + data.Withdrawable);
+      $('#menu-play-balance').text(' ' + data.Balance);
+
+
+  } else if (data.LogOut == "1") {
+      this.logOutCount++;
+      if (this.logOutCount <= 2) {
+          this.getBalanceAmount();
+      }
+      if (this.logOutCount > 2) {
+          document.location.reload();
+      }
+  }
+
+
+  //desktop message
+
+  $('.msg--count').remove();
+  $('#msg--count').remove();
+  if (data.InboxCount > 0) {
+      var $messageViewer = $('.Button--messages .msg--holder');
+      var $menuMessageViewer = $('#msg-row');
+
+      if ($messageViewer.length) {
+          $messageViewer.append('<span class="msg--count">' + data.InboxCount + '</span>');
+          $menuMessageViewer.append('<span id="msg--count" class="msg--count">' + data.InboxCount + '</span>');
+      } else {
+          $menuMessageViewer.append('<span class="msg--count">' + data.InboxCount + '</span>');
+      }
+  } 
+}
+};
